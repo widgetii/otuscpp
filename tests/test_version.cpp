d8f73d485ed034e7c6dcafe56abcac66c30420f6
Dmitry Ilyin <dima@doty.ru>
2017-12-10 21:03:04 +0300

    added test to cmake

--- tests/test_version.cpp
+++ tests/test_version.cpp
@@ -9,7 +9,7 @@ BOOST_AUTO_TEST_SUITE(test_version)
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
 //    BOOST_CHECK(version() > 0);
-    BOOST_CHECK(true);
+    BOOST_CHECK(1 > 0);
 }
 
-}
+BOOST_AUTO_TEST_SUITE_END()
